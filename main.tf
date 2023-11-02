7add3499fbd888159dfae36d25ea75be570b3d24
Oluwadamilola Ijato <oluwadamilola.ijato@postcode-lotterie.de>
2023-11-02 19:19:08 +0100

    creating a static s3 website

---
+++ main.tf
@@ -0,0 +1,49 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.3.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+}
+
+
+
+
+# Create S3 Bucket Resource
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+  acl    = "public-read"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Sid": "PublicReadGetObject",
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": [
+              "s3:GetObject"
+          ],
+          "Resource": [
+              "arn:aws:s3:::${var.bucket_name}/*"
+          ]
+      }
+  ]
+}  
+EOF
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+  tags          = var.tags
+  force_destroy = true
+}
+
