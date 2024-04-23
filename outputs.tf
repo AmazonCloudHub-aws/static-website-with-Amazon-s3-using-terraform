92b6d5e99e2cbf1ae2f0b2db29eadc1075ec9b76
dhamsey3 <73405591+dhamsey3@users.noreply.github.com>
2024-04-23 12:26:06 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -19,3 +19,7 @@ output "endpoint" {
   description = "Endpoint Information of the bucket"
   value       = aws_s3_bucket.s3_bucket.website_endpoint
 }
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
